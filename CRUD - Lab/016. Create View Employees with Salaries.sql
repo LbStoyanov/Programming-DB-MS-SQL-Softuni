e378b3d0cbd81423d9cc9f8cf1af9cdc39835d9b
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-06-28 00:29:15 +0300

    S

---
+++ CRUD - Lab/016. Create View Employees with Salaries.sql
@@ -0,0 +1,9 @@
+﻿CREATE VIEW [dbo].[V_EmployeeNameJobTitle]
+AS
+SELECT 
+CONCAT(FirstName, ' ',ISNULL(MiddleName, ''), LastName) AS [Full Name], 
+
+JobTitle AS [Job Title]
+FROM 
+dbo.Employees
+GO
