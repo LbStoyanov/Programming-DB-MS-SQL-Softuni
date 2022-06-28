df516ce473fa0cfb3c689221219e99c2e39e4083
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-06-28 20:54:50 +0300

    d

--- Table Relations/Table Relations.sql
+++ Table Relations/Table Relations.sql
@@ -12,11 +12,40 @@ CREATE TABLE [Persons](
 --ADD [PassportID] INT NOT NULL
 
 
-CREATE TABLE[Passport](
-		[PassportID] INT FOREIGN KEY REFERENCES Persons(PersonID),
-		[PassportNumber] NVARCHAR(20)
 
-);
+CREATE TABLE [dbo].[Passports](
+	[PassportID] [int] NOT NULL,
+	[PassportNumber] [nvarchar](20) NOT NULL,
+ CONSTRAINT [PK_Passports] PRIMARY KEY CLUSTERED 
+(
+	[PassportID] ASC
+)
+)
+CREATE TABLE [dbo].[Persons](
+	[PersonID] [int] NOT NULL,
+	[FirstName] [nvarchar](50) NOT NULL,
+	[Salary] [decimal](18, 2) NOT NULL,
+	[PassportID] [int] NOT NULL,
+ CONSTRAINT [PK_Persons] PRIMARY KEY CLUSTERED 
+(
+	[PersonID] ASC
+)
+)
 
-INSERT INTO [Persons] 
-VALUES(1, 'Roberto', 43300.00, 102)
+INSERT [dbo].[Passports] ([PassportID], [PassportNumber]) VALUES (101, N'N34FG21B')
+
+INSERT [dbo].[Passports] ([PassportID], [PassportNumber]) VALUES (102, N'K65LO4R7')
+
+INSERT [dbo].[Passports] ([PassportID], [PassportNumber]) VALUES (103, N'ZE657QP2')
+
+INSERT [dbo].[Persons] ([PersonID], [FirstName], [Salary], [PassportID]) VALUES (1, N'Roberto     ', CAST(43300.00 AS Decimal(18, 2)), 102)
+
+INSERT [dbo].[Persons] ([PersonID], [FirstName], [Salary], [PassportID]) VALUES (2, N'Tom', CAST(56100.00 AS Decimal(18, 2)), 103)
+
+INSERT [dbo].[Persons] ([PersonID], [FirstName], [Salary], [PassportID]) VALUES (3, N'Yana', CAST(60200.00 AS Decimal(18, 2)), 101)
+
+ALTER TABLE [dbo].[Persons]  WITH CHECK ADD  CONSTRAINT [FK_Persons_Passports] FOREIGN KEY([PassportID])
+REFERENCES [dbo].[Passports] ([PassportID])
+
+ALTER TABLE [dbo].[Persons] CHECK CONSTRAINT [FK_Persons_Passports]
+
