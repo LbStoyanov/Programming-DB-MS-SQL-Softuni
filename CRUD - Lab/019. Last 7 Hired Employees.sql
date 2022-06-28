747791545c7741880dc3db2a9f9cc0f764fc8329
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-06-28 18:13:53 +0300

    CRUD - Lab done!

--- CRUD - Lab/019. Last 7 Hired Employees.sql
+++ CRUD - Lab/019. Last 7 Hired Employees.sql
@@ -41,4 +41,38 @@ WHERE DepartmentID = @EngineeringID
 SELECT Salary
 FROM Employees;
 
---
+-- 22. All Mountain Peaks
+
+SELECT [PeakName]
+FROM [Peaks]
+ORDER BY [PeakName]
+
+--23. Biggest Countries by Population
+
+SELECT TOP (30) [CountryName],[Population] 
+FROM [Countries]
+WHERE
+[ContinentCode] = 'EU'
+ORDER BY
+[Population] DESC
+
+--24. Countries and Currency (Euro / Not Euro)
+
+SELECT CountryName,
+       CountryCode,
+       CASE CurrencyCode
+           WHEN 'EUR'
+           THEN 'Euro'
+           ELSE 'Not Euro'
+       END AS 'Currency'
+FROM Countries
+ORDER BY CountryName;
+
+--25. All Diablo Characters
+
+SELECT
+[Name]
+FROM
+[Characters]
+ORDER BY
+[Name]
