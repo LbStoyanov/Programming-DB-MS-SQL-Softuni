78036079f0dfdc74e208f0f104826230d0931c83
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-06-29 12:13:56 +0300

    UniversityDatabase

---
+++ Table Relations/Problem 6. University Database.sql
@@ -0,0 +1,31 @@
+﻿CREATE TABLE [Subjects](
+	[SubjectID] INT PRIMARY KEY NOT NULL,
+	[SubjectName] NVARCHAR(50) NOT NULL 
+)
+
+CREATE TABLE [Majors](
+	[MajorID] INT PRIMARY KEY IDENTITY NOT NULL,
+	[Name] NVARCHAR(50) NOT NULL
+)
+
+CREATE TABLE [Students](
+	[StudentID] INT PRIMARY KEY IDENTITY NOT NULL,
+	[StudentNumber] INT NOT NULL,
+	[StudentName] NVARCHAR(50) NOT NULL,
+	[MajorID] INT FOREIGN KEY REFERENCES [Majors](MajorID) NOT NULL
+
+)
+
+CREATE TABLE [Agenda](
+	[StudentID] INT FOREIGN KEY REFERENCES [Students](StudentID),
+	[SubjectID] INT FOREIGN KEY REFERENCES [Subjects](SubjectID),
+	CONSTRAINT PK_AgendaId PRIMARY KEY(StudentID,SubjectID)
+)
+
+
+CREATE TABLE [Payments](
+	[PaymentID] INT PRIMARY KEY IDENTITY NOT NULL,
+	[PaymentDate] DATETIME2,
+	[PaymentAmount] DECIMAL NOT NULL,
+	[StudentID] INT FOREIGN KEY REFERENCES [Students](StudentID)
+)
