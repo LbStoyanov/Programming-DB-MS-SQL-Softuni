9d4dd6f6a2423eb75b447465f378402dc1901c26
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-06-30 08:49:00 +0300

    Built-In-Functions - Exercise

---
+++ Built-in Functions/Built-In-Funtions.sql
@@ -0,0 +1,14 @@
+﻿--01. Find Names of All Employees by First Name
+
+SELECT FirstName,LastName
+FROM Employees
+WHERE FirstName LIKE 'Sa%'--Starts With
+
+--02. Find Names of All Employees by Last Name
+
+SELECT FirstName,LastName
+FROM Employees
+WHERE LastName LIKE '%ei%' --Contains
+
+--03. Find First Names of All Employess
+
