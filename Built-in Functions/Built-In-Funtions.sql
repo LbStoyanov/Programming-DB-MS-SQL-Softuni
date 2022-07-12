e51357d6a19f7f5f218a19c11acb5b4451a8ff3d
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-07-12 16:59:58 +0300

    S

--- Built-in Functions/Built-In-Funtions.sql
+++ Built-in Functions/Built-In-Funtions.sql
@@ -98,5 +98,12 @@ FROM [Countries]
 WHERE [CountryName] LIKE '%a%a%a%'
 ORDER BY [IsoCode]
 
+--13. Mix of Peak and River Names
 
+SELECT Peaks.PeakName,
+       Rivers.RiverName,
+       LOWER(CONCAT(LEFT(Peaks.PeakName, LEN(Peaks.PeakName)-1), Rivers.RiverName)) AS Mix
+FROM Peaks
+     JOIN Rivers ON RIGHT(Peaks.PeakName, 1) = LEFT(Rivers.RiverName, 1)
+ORDER BY Mix;
 
