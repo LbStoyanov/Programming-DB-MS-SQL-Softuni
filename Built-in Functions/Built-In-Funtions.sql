10d7e36482cf1c8a48aadafa7296e4382ddc8286
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-07-13 11:57:35 +0300

    Built-in Functions

--- Built-in Functions/Built-In-Funtions.sql
+++ Built-in Functions/Built-In-Funtions.sql
@@ -126,7 +126,34 @@ FROM [Games]
 WHERE YEAR([Start]) IN(2011,2012)
 ORDER BY [Start],[Name]
 
+--15. User Email Providers
 
+--16. Get Users with IPAddress Like Pattern
+
+--17. Show All Games with Duration
+
+SELECT  [Name]
+	AS	[Game],
+		CASE
+			WHEN DATEPART(HOUR, [Start]) >= 0 AND DATEPART(HOUR, [Start]) < 12 THEN  'Morning'
+			WHEN DATEPART(HOUR, [Start]) >= 12 AND DATEPART(HOUR, [Start]) < 18 THEN  'Afternoon'
+			ELSE 'Evening'
+		END AS [Part of the Day],
+		CASE	
+			WHEN [Duration] <= 3 THEN 'Extra Short'
+			WHEN [Duration] BETWEEN 4 AND 6 THEN 'Short'
+			WHEN [Duration] > 6 THEN 'Long'
+			ELSE 'Extra Long'
+	    END AS [Duration]
+	   
+FROM [Games]
+ORDER BY [Game],[Duration],[Part of the Day]
+
+
+
+
+
+--18. Orders Table
 
 
 
