c1063c4841afe8775bbfebf703241ee6d2dfc575
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-07-13 11:06:06 +0300

    Built-In-Funcs

--- Built-in Functions/Built-In-Funtions.sql
+++ Built-in Functions/Built-In-Funtions.sql
@@ -118,6 +118,18 @@ ORDER BY [Mix]
 
 --14. Games From 2011 and 2012 Year
 
+SELECT TOP(50) [Name],
+		FORMAT(CAST([Start] AS DATE),'yyyy-MM-dd') AS [Start]
+
+FROM [Games]
+
+WHERE YEAR([Start]) IN(2011,2012)
+ORDER BY [Start],[Name]
+
+
+
+
+
 
 
 
