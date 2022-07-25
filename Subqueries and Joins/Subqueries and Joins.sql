2517f065f70c778d9b8cd0a8321889242771b4c9
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-07-25 13:30:13 +0300

    JoinsAndSubQueries

--- Subqueries and Joins/Subqueries and Joins.sql
+++ Subqueries and Joins/Subqueries and Joins.sql
@@ -10,3 +10,33 @@ FROM [Employees] AS e
 INNER JOIN [Addresses] AS a ON e.AddressID = a.AddressID
 INNER JOIN [Towns] AS t ON a.TownID = t.TownID
 ORDER BY e.FirstName, e.LastName
+
+--1.	Employee Address
+	--Create a query that selects:
+		--EmployeeId
+		--JobTitle
+		--AddressId
+		--AddressText
+		--Return the first 5 rows sorted by AddressId in ascending order.
+SELECT TOP(5)
+	e.EmployeeID,
+	e.JobTitle,
+	a.AddressID,
+	a.AddressText
+
+FROM [Employees] AS e
+JOIN [Addresses] AS a ON e.AddressID = a.AddressID
+ORDER BY a.AddressID
+
+--02. Addresses with Towns
+
+	--Write a query that selects:
+		--•	FirstName
+		--•	LastName
+		--•	Town
+		--•	AddressText
+		--Sorted by FirstName in ascending order then by LastName. Select first 50 employees.
+
+
+
+
