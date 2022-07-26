2980770261929283a0999da80a829065c7739ce4
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-07-26 20:10:36 +0300

    JoinsAndSubqueries - Exerceize - Done!

--- Subqueries and Joins/Subqueries and Joins.sql
+++ Subqueries and Joins/Subqueries and Joins.sql
@@ -325,3 +325,29 @@ FROM (
 		FROM Countries AS c
 		GROUP BY c.ContinentCode, c.CurrencyCode) AS rankedCurrencies
 WHERE rankedCurrencies.rank = 1 and rankedCurrencies.Count > 1
+
+
+--16. Countries Without any Mountains
+
+	--Create all the count of all countries, which don’t have a mountain.
+
+--17. Highest Peak and Longest River by Country
+
+	--For each country, find the elevation of the highest peak and the length of the longest river, 
+	--sorted by the highest peak elevation (from highest to lowest), 
+	--then by the longest river length (from longest to smallest), then by country name (alphabetically). 
+	--Display NULL when no data is available in some of the columns. Limit only the first 5 rows.
+
+
+SELECT TOP(5)
+	c.[CountryName]
+	,MAX(p.[Elevation]) AS [HighestPeakElevation]
+	,MAX(r.[Length]) AS [LongestRiverLength]
+FROM [Countries] AS c
+LEFT JOIN [MountainsCountries] AS mc ON c.[CountryCode] = mc.[CountryCode]
+LEFT JOIN [Mountains] AS m ON m.[Id] = mc.[MountainId]
+LEFT JOIN [Peaks] AS p ON p.[MountainId] = m.[Id]
+LEFT JOIN [CountriesRivers] AS cr ON c.[CountryCode] = cr.[CountryCode]
+LEFT JOIN [Rivers] AS r ON r.[Id] = cr.[RiverId]
+GROUP BY c.[CountryName]
+ORDER BY [HighestPeakElevation] DESC, [LongestRiverLength] DESC, [CountryName]
