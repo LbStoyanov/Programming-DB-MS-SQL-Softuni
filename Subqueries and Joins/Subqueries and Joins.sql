ec15b00b63fb013f41b024582980aeb37a711268
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-07-26 14:57:13 +0300

    sql

--- Subqueries and Joins/Subqueries and Joins.sql
+++ Subqueries and Joins/Subqueries and Joins.sql
@@ -314,3 +314,14 @@ ORDER BY c.[CountryName]
 		--Find all continents and their most used currency. Filter any currency that is used in only one country. Sort your results by ContinentCode.
 
 
+SELECT 
+	rankedCurrencies.ContinentCode
+	, rankedCurrencies.CurrencyCode
+	, rankedCurrencies.Count
+FROM (
+		SELECT c.ContinentCode,
+			c.CurrencyCode, COUNT(c.CurrencyCode) AS [Count]
+			, DENSE_RANK() OVER (PARTITION BY c.ContinentCode ORDER BY COUNT(c.CurrencyCode) DESC) AS [rank] 
+		FROM Countries AS c
+		GROUP BY c.ContinentCode, c.CurrencyCode) AS rankedCurrencies
+WHERE rankedCurrencies.rank = 1 and rankedCurrencies.Count > 1
