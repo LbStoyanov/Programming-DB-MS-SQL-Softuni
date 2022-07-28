9a050035ce853b80ee6dd571d5a7635b87f8a8f7
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-07-28 13:56:08 +0300

    Idexes MSSQL

---
+++ Indices and Data Aggregation - Lab/IndicesAndDataAggregationLab.sql
@@ -0,0 +1,17 @@
+﻿--1. Records’ Count
+--Import the database and send the total count of records from the one and only table to Mr. Bodrog. Make sure nothing got lost.
+
+SELECT COUNT(*) 
+     FROM dbo.WizzardDeposits
+
+
+--02. Longest Magic Wand
+
+--Select the size of the longest magic wand. Rename the new column appropriately.
+
+SELECT MAX([MagicWandSize]) AS LongestMagicWand
+     FROM [WizzardDeposits] 
+
+
+--03. Longest Magic Wand per Deposit Groups
+    --For wizards in each deposit group show the longest magic wand. Rename the new column appropriately.
