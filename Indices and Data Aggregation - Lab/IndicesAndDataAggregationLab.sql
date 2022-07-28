b79f8ce418ad203c521591f571a29a0f9d8d9a47
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-07-28 20:15:41 +0300

    GroupingMSSQL

--- Indices and Data Aggregation - Lab/IndicesAndDataAggregationLab.sql
+++ Indices and Data Aggregation - Lab/IndicesAndDataAggregationLab.sql
@@ -60,7 +60,7 @@ GROUP BY [DepositGroup]
 
 SELECT
 	[DepositGroup],
-	SUM([DepositAmount]) AS TotalSum
+	SUM([DepositAmount]) AS [TotalSum]
 FROM [WizzardDeposits]
 WHERE [MagicWandCreator] = 'Ollivander family'
 GROUP BY [DepositGroup]
@@ -68,4 +68,29 @@ HAVING SUM([DepositAmount]) < 150000
 ORDER BY [TotalSum] DESC
 
 
---
+--08. Deposit Charge
+
+--09. Age Group
+
+SELECT 
+	[AgeGroup]
+	,COUNT([Id]) AS [WizzardCount]
+	
+FROM 
+(
+	SELECT
+		*,
+		CASE
+			WHEN [Age] BETWEEN 0 AND 10 THEN '[0-10]'
+			WHEN [Age] BETWEEN 11 AND 20 THEN '[11-20]'
+			WHEN [Age] BETWEEN 21 AND 30 THEN '[21-30]'
+			WHEN [Age] BETWEEN 30 AND 40 THEN '[31-40]'
+			WHEN [Age] BETWEEN 41 AND 50 THEN '[41-50]'
+			WHEN [Age] BETWEEN 51 AND 60 THEN '[51-60]'
+			ELSE '[61+]'
+		END AS [AgeGroup]
+	FROM [WizzardDeposits]
+) AS [AgeGroupQuerie]
+GROUP BY [AgeGroup]
+
+
