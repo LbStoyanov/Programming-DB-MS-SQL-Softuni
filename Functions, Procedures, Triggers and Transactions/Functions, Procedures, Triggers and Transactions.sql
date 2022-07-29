9156ed90b2ac6befb2756b39dad3c335b5c89075
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-07-29 15:32:30 +0300

    MSSQ-Stored Procedures

---
+++ Functions, Procedures, Triggers and Transactions/Functions, Procedures, Triggers and Transactions.sql
@@ -0,0 +1,6 @@
+﻿--01. Employees with Salary Above 35000
+
+		--Create stored procedure usp_GetEmployeesSalaryAbove35000 that returns all employees’ first and last names for whose 
+		--salary is above 35000. 
+
+
