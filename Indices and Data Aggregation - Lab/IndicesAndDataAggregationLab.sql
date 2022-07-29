4045766ce1e4b30c1e6407cae2cdd2d4b3ece298
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-07-29 11:20:58 +0300

    MSSQL

--- Indices and Data Aggregation - Lab/IndicesAndDataAggregationLab.sql
+++ Indices and Data Aggregation - Lab/IndicesAndDataAggregationLab.sql
@@ -94,3 +94,17 @@ FROM
 GROUP BY [AgeGroup]
 
 
+--10. First Letter
+  --Create a query that returns all unique wizard first letters of their first names only if they have deposit of type Troll Chest.
+  --Order them alphabetically. Use GROUP BY for uniqueness.
+
+SELECT 
+	LEFT([FirstName],1) AS [FirstLetter]
+	
+		 FROM [WizzardDeposits]
+		WHERE [DepositGroup] = 'Troll Chest'
+	GROUP BY LEFT([FirstName],1)
+	ORDER BY [FirstLetter]
+
+
+
