1b75381657061a875e490e7ed22775b84e325dbb
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-08-01 21:21:41 +0300

    Functions MSSQL

--- Functions, Procedures, Triggers and Transactions/Functions, Procedures, Triggers and Transactions.sql
+++ Functions, Procedures, Triggers and Transactions/Functions, Procedures, Triggers and Transactions.sql
@@ -72,3 +72,30 @@ END
 	--•	If salary is < 30000 return "Low"
 	--•	If salary is between 30000 and 50000 (inclusive) return "Average"
 	--•	If salary is > 50000 return "High"
+
+
+CREATE FUNCTION ufn_GetSalaryLevel (@salary DECIMAL(18 , 4))
+RETURNS VARCHAR(7)
+AS
+BEGIN
+	DECLARE @salaryLevel VARCHAR(7)
+
+	IF @salary < 30000
+	BEGIN
+		SET @salaryLevel = 'Low'
+	END
+	ELSE IF @salary BETWEEN 30000 AND 50000
+	BEGIN
+		SET @salaryLevel = 'Average'
+	END
+	ELSE
+	BEGIN
+		SET @salaryLevel = 'High'
+	END
+
+	RETURN @salaryLevel
+END
+
+
+--06. Employees by Salary Level
+
