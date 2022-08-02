f4a84a590c053d040939a96bc37da44370917d0e
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-08-02 09:56:24 +0300

    sql

--- Functions, Procedures, Triggers and Transactions/Functions, Procedures, Triggers and Transactions.sql
+++ Functions, Procedures, Triggers and Transactions/Functions, Procedures, Triggers and Transactions.sql
@@ -120,3 +120,32 @@ END
 
 	--Define a function ufn_IsWordComprised(@setOfLetters, @word) 
 	--that returns true or false depending on that if the word is comprised of the given set of letters. 
+
+CREATE FUNCTION ufn_IsWordComprised(@setOfLetters VARCHAR (50), @word VARCHAR (50)) 
+RETURNS BIT
+AS
+BEGIN
+     DECLARE @index INT = 1
+     DECLARE @length INT = LEN(@word)
+     DECLARE @letter CHAR(1)
+
+     WHILE (@index <= @length)
+     BEGIN
+          SET @letter = SUBSTRING(@word, @index, 1)
+          IF (CHARINDEX(@letter, @setOfLetters) > 0)
+             SET @index = @index + 1
+          ELSE
+             RETURN 0
+     END
+     RETURN 1
+END 
+
+
+--08.Delete Employees and Departments
+
+	--Create a procedure with the name usp_DeleteEmployeesFromDepartment (@departmentId INT) which deletes all Employees from a given 
+	--department. Delete these departments from the Departments table too. 
+	--Finally, SELECT the number of employees from the given department. If the delete statements are correct the select query should return 0.
+	--After completing that exercise restore your database to revert all changes.
+	
+	
