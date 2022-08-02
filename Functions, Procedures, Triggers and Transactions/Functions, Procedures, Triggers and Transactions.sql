b4beda541163074e6fdf092c4721026e5686271a
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-08-02 10:32:25 +0300

    mssql queries with storedProcedures

--- Functions, Procedures, Triggers and Transactions/Functions, Procedures, Triggers and Transactions.sql
+++ Functions, Procedures, Triggers and Transactions/Functions, Procedures, Triggers and Transactions.sql
@@ -148,4 +148,43 @@ END
 	--Finally, SELECT the number of employees from the given department. If the delete statements are correct the select query should return 0.
 	--After completing that exercise restore your database to revert all changes.
 	
+	CREATE PROCEDURE usp_DeleteEmployeesFromDepartment (@departmentId INT) 
+AS
+BEGIN
+	DELETE FROM [EmployeesProjects]
+	WHERE [EmployeeID] IN (
+								SELECT [EmployeeID]
+								FROM [Employees]
+								WHERE [DepartmentID] = @departmentId
+						  )	
+
+	UPDATE [Employees]
+	SET [ManagerID] = NULL
+	WHERE [ManagerID] IN (
+								SELECT [EmployeeID]
+								FROM [Employees]
+								WHERE [DepartmentID] = @departmentId
+						 )	
+
+	ALTER TABLE [Departments]
+	ALTER COLUMN [ManagerID] INT
 	
+	UPDATE [Departments]
+	SET [ManagerID] = NULL
+	WHERE [ManagerID] IN (
+								SELECT [EmployeeID]
+								FROM [Employees]
+								WHERE [DepartmentID] = @departmentId
+						 )
+
+	DELETE FROM [Employees]
+	WHERE [DepartmentID] = @departmentId
+
+	DELETE FROM [Departments]
+	WHERE[DepartmentID] = @departmentId
+
+	SELECT
+		COUNT(*)
+	FROM [Employees]
+	WHERE [DepartmentID] = @departmentId
+END
