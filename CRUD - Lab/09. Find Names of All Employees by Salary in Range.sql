61b8f0d8cacb91308fb1b3a93b94cb8f8c55ee46
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2023-01-23 20:21:33 +0200

    CRUD

--- CRUD - Lab/09. Find Names of All Employees by Salary in Range.sql
+++ CRUD - Lab/09. Find Names of All Employees by Salary in Range.sql
@@ -1,1 +1,1 @@
-﻿  SELECT [FirstName],[LastName],JobTitle  FROM Employees WHERE [Salary] >= 20000 AND [Salary] <= 30000
+﻿SELECT [FirstName],[LastName],JobTitle  FROM Employees WHERE [Salary] >= 20000 AND [Salary] <= 30000
