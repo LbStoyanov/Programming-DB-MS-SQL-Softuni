38ac91828f2592ca96223dfbdd373982f652abda
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2023-02-07 20:45:06 +0200

    ExamPrep

--- ExamPrep/Exam1/Exam1/Exam1/Exam1.sql
+++ ExamPrep/Exam1/Exam1/Exam1/Exam1.sql
@@ -15,7 +15,6 @@ CREATE TABLE Owners
 	[Address] VARCHAR(50) 
 );
 
-
 CREATE TABLE AnimalTypes
 (
 	Id INT PRIMARY KEY IDENTITY,
@@ -31,54 +30,52 @@ CREATE TABLE Cages
 CREATE TABLE Animals
 (
 	Id INT PRIMARY KEY IDENTITY,
-	Name VARCHAR(30) NOT NULL,
+	[Name] VARCHAR(30) NOT NULL,
 	BirthDate DATE NOT NULL,
 	OwnerId INT FOREIGN KEY REFERENCES Owners (Id),
 	AnimalTypeId INT FOREIGN KEY REFERENCES AnimalTypes (Id) NOT NULL,
 )
 
-CREATE TABLE Animals
+CREATE TABLE AnimalsCages
 (
-	Id INT PRIMARY KEY IDENTITY,
-	Name VARCHAR(30) NOT NULL,
-	BirthDate DATE NOT NULL,
-	OwnerId INT FOREIGN KEY REFERENCES Owners (Id),
-	AnimalTypeId INT FOREIGN KEY REFERENCES AnimalTypes (Id) NOT NULL,
-)
-
-
-CREATE TABLE AnimalCages
-(
-	CageId INT FOREIGN KEY REFERENCES Cafes(Id),
-	AnimalId INT FOREIGN KEY REFERENCES Animals(Id),
+	CageId INT FOREIGN KEY REFERENCES Cages(Id) NOT NULL,
+	AnimalId INT FOREIGN KEY REFERENCES Animals(Id) NOT NULL,
 	PRIMARY KEY(CageId, AnimalId)
 )
 
 CREATE TABLE VolunteersDepartments
 (
 	Id INT PRIMARY KEY IDENTITY,
-	DepartmentName VARCHAR(30) NOT NULL,
+	DepartmentName VARCHAR(30) NOT NULL
 )
 
 CREATE TABLE Volunteers
 (
 	Id INT PRIMARY KEY IDENTITY,
-	Name VARCHAR(50) NOT NULL,
+	[Name] VARCHAR(50) NOT NULL,
 	PhoneNumber VARCHAR(15) NOT NULL,
-	Address VARCHAR(50),
+	[Address] VARCHAR(50),
 	AnimalId INT FOREIGN KEY REFERENCES Animals(Id),
-	DepartmentId INT FOREIGN KEY REFERENCES VolunteersDepartments(Id)
+	DepartmentId INT FOREIGN KEY REFERENCES VolunteersDepartments(Id) NOT NULL
 )
 
 
 
 --Task 02.
 
-INSERT INTO Animals
-([Name],BirthDate,OwnerId,AnimalTypeId
+INSERT INTO Animals ([Name], BirthDate, OwnerId, AnimalTypeId)
 VALUES
-('Giraffe', '2018-09-21', 21, 1),
-('Harpy Eagle', '2018-09-21', 15, 3),
-('Giraffe', '2018-09-21', 21, 1),
-('Giraffe', '2018-09-21', 21, 1),
+('Giraffe', '2018-09-21',21 ,1),
+('Harpy Eagle', '2015-04-17',15 ,3),
+('Hamadryas Baboon', '2017-11-02',NULL ,1),
+('Tuatara', '2021-06-30',2 ,4)
+
+
+INSERT INTO Volunteers ([Name], PhoneNumber, [Address], AnimalId, DepartmentId)
+VALUES
+('Anita Kostova', '0896365412','Sofia, 5 Rosa str.', 15, 1),
+('Dimitur Stoev', '0877564223',NULL, 42, 4),
+('Kalina Evtimova', '0896321112','Silistra, 21 Breza str.', 9, 7),
+('Stoyan Tomov', '0898564100','Montana, 1 Bor str.', 18, 8),
+('Boryana Mileva', '0888112233',NULL, 31, 5);
 
