31bb7f5c20432cd1fddf4000fcb6fef8d9ffcac2
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2023-02-15 20:47:05 +0200

    ExamPrep

--- ExamPrep/Exam1/Exam1/Exam1/Exam1.sql
+++ ExamPrep/Exam1/Exam1/Exam1/Exam1.sql
@@ -165,4 +165,11 @@ SELECT dbo.udf_GetVolunteersCountFromADepartment ('Education program assistant')
 
 --12.Animals with Owner or Not
 
-
+CREATE PROCEDURE usp_AnimalsWithOwnersOrNot(@AnimalName VARCHAR(30))
+AS
+BEGIN
+						    SELECT   a.[Name]
+									,ISNULL(o.[Name], 'For adoption') AS OwnersName
+							FROM Animals AS a LEFT JOIN Owners AS o ON a.OwnerId = o.Id
+							WHERE a.[Name] = @AnimalName
+END
